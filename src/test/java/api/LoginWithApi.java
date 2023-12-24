8cc5b879167aaf3e5102544f707280ba32672e5f
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-12-24 16:48:33 +0300

    first commit

--- src/test/java/api/LoginWithApi.java
+++ src/test/java/api/LoginWithApi.java
@@ -1,4 +1,26 @@
 package api;
 
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import models.SuccessfulRegisterBodyModel;
+
+import static io.restassured.RestAssured.given;
+import static spec.RegAndResSpec.*;
+
 public class LoginWithApi {
+    @Step("Authorization")
+    public Response getAuth() {
+        SuccessfulRegisterBodyModel successfulRegisterBodyModel = new SuccessfulRegisterBodyModel();
+        successfulRegisterBodyModel.setUserName("Bteeny");
+        successfulRegisterBodyModel.setPassword("Test(111)^");
+        return given(requestSpec)
+                .body(successfulRegisterBodyModel)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .spec(responseSpec)
+                .statusCode(200)
+                .extract().response();
+    }
+
 }
---
+++ src/test/java/helpers/CookieBase.java
@@ -0,0 +1,23 @@
+package helpers;
+
+import api.LoginWithApi;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+
+public class CookieBase implements BeforeEachCallback {
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        LoginWithApi loginWithApi = new LoginWithApi();
+        Response cookie = loginWithApi.getAuth();
+        open("/images/Toolsqa.jpg");
+        getWebDriver().manage().addCookie(new Cookie("userID", cookie.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", cookie.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", cookie.path("token")));
+    }
+}
