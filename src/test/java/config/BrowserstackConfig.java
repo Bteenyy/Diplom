714b8c416684690bfc3697f01f12f71a68d827d8
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2024-01-08 18:32:48 +0300

    first commit

---
+++ src/test/java/config/BrowserstackConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${deviceHost}.properties"
+})
+public interface BrowserstackConfig extends Config {
+    @Key("browserstack.userName")
+    String getUserName();
+
+    @Key("browserstack.accessKey")
+    String getAccessKey();
+
+
+    @Key("browserstack.url")
+    String getUrl();
+
+    @Key("browserstack.device")
+    String getDevice();
+
+    @Key("browserstack.os_version")
+    String getOS();
+
+    @Key("browserstack.app")
+    String getApp();
+}
