85d7cd816f5a26f3e6b190d3a41763591d3a660f
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2024-01-16 10:27:34 +0300

    first commit

--- src/test/java/helpers/TestData.java
+++ src/test/java/helpers/TestData.java
@@ -4,7 +4,7 @@ import com.github.javafaker.Faker;
 
 public class TestData {
     Faker faker = new Faker();
-    public String email = "rasitsahbutdinov915455@gmail.com";
+    public String email = "rashitsahbutdinov@yandex.ru";
     public String password = "mdf9MsZs2bbM7kq_";
     public String randomEmail = faker.internet().emailAddress();
     public String randomPassword = faker.random().hex(6);
